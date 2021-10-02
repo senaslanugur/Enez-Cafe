f321a5a5575bc34248e918c095ce111242ec331e
senaslanugur <91412306+senaslanugur@users.noreply.github.com>
2021-10-02 23:06:42 +0300

    Add files via upload

---
+++ js/ana_yemekler.js
@@ -0,0 +1,9 @@
+  var dat = data
+  var hst = document.getElementById("highscore");
+  for(var k = 0; k < dat.prodcuts.length; k++){
+    hst.innerHTML += '<div class="col mb-5"> <div class="card h-100"><img class="card-img-top" src="'+(dat.prodcuts[k].photo_name)+'" alt="..." /><div class="card-body p-4"><div class="text-center"><h6 class="fw-bolder">'
+    +(dat.prodcuts[k].manu_name)+'</h6>'+(dat.prodcuts[k].details)+'<p>'+(dat.prodcuts[k].price)+' TL</p></div></div><div class="card-footer p-4 pt-0 border-top-0 bg-transparent"><div class="text-center"><button type="button" class="btn btn-primary" onclick="test()">Ekle</button></div>'+
+    '</div></div></div>'}
+function test(){
+	alert("test	")
+}
---
+++ js/data.js
@@ -0,0 +1,74 @@
+var data = 
+{
+  "prodcuts":[
+    {
+      "manu_name":"Hamburger",
+      "details":"Patates Kızartması ve Icecek",
+      "price":30,
+      "photo_name":"https://www.soke.com.tr/Files/tarifler/HamburgerEkmegi3.jpg"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    },
+    {
+      "manu_name":"Karısık Pizza",
+      "details":"Patates Kızartması ve Icecek",
+      "price":25,
+      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+    }
+    ]
+  
+}
+
+
