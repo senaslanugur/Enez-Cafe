77a11e035fcd6a90ffebd925651f09b9584275e8
senaslanugur <91412306+senaslanugur@users.noreply.github.com>
2021-10-07 21:40:54 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,74 +1,42 @@
-var data = 
+var data =
 {
   "prodcuts":[
     {
-      "manu_name":"Hamburger",
+      "manu_name":"Karışık Tost Menü",
+      "details":"Patates Kızartması ve İçecek",
+      "price":25,
+      "photo_name":"http://www.salmanpastanesi.com/Assets/Image/urunler/tost/karisik.png"
+    },
+    {
+      "manu_name":"Kavurmalı Tost Menü",
       "details":"Patates Kızartması ve Icecek",
       "price":30,
-      "photo_name":"https://www.soke.com.tr/Files/tarifler/HamburgerEkmegi3.jpg"
+      "photo_name":"https://www.balgatsimitdunyasi.com/image/cache/catalog/urun/kasarli-yumurtali-kavurmali-bazlama-tost-ba83-550x550.jpg"
     },
     {
-      "manu_name":"Karısık Pizza",
+      "manu_name":"Hamburger Menü",
       "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+      "price":35,
+      "photo_name":"https://cevatusta.com.tr/wp-content/uploads/2020/12/hamburger-600x400.jpg"
     },
     {
-      "manu_name":"Karısık Pizza",
+      "manu_name":"Cheeseburger Menü",
       "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+      "price":38,
+      "photo_name":"https://www.demo45.hazirwebsiteler.com/Uploads/Products/thumb/xlg_cheesburher.jpg"
     },
     {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+      "manu_name":"Patates Kızartması",
+      "details":"Patates Kızartması",
+      "price":20,
+      "photo_name":"https://i2.milimaj.com/i/milliyet/75/0x410/5f99585cadcdeb046890eb07.jpg"
     },
     {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
-    },
-    {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
-    },
-    {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
-    },
-    {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
-    },
-    {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
-    },
-    {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
-    },
-    {
-      "manu_name":"Karısık Pizza",
-      "details":"Patates Kızartması ve Icecek",
-      "price":25,
-      "photo_name":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThx_zlsXjKCGXgqX1GCKDn6B5M58Es15E5ig&usqp=CAU"
+      "manu_name":"Soğan Halkası",
+      "details":"Soğan Halkası",
+      "price":15,
+      "photo_name":"https://cdn.yemek.com/mncrop/940/625/uploads/2019/12/kolay-sogan-halkasi-tarifi.jpg"
     }
     ]
-  
+
 }
-
-
