4b940161188ed75c0ac0bb3294f3d0afc510a703
senaslanugur <91412306+senaslanugur@users.noreply.github.com>
2021-10-11 00:20:51 +0300

    Add files via upload

--- js/siparis_ver.js
+++ js/siparis_ver.js
@@ -1,3 +1,4 @@
+
 var goruntu = document.getElementById("siparis_tamamla");
 goruntu.innerHTML = localStorage.length
 
@@ -13,7 +14,7 @@ var toplam = 0;
 for(var k = 0; k < keys.length; k++){
   var bilgiler = JSON.parse(`${localStorage.getItem(keys[k])}`);
   son_odeme.innerHTML += '<tr><td class="col-md-9"><em>'+keys[k]+'</em> </td><td class="col-md-1" style="text-align: center">'+(bilgiler.urun_adedi)+'</td>  <td class="col-md-1 text-center">&#8378;\
-  '+(bilgiler.urun_fiyati)+'</td><td class="col-md-1 text-center">&#8378;'+(bilgiler.urun_toplami)+'</td></td><td class="col-md-1 text-center"><button class="btn btn-outline-danger" id="'+keys[k]+' "onclick="sil(this)"><i class="fa fa-trash"></i></button></td></tr>'
+  '+(bilgiler.urun_fiyati)+'</td><td class="col-md-1 text-center">&#8378;'+(bilgiler.urun_toplami)+'</td></td><td class="col-md-1 text-center"><button class="btn btn-outline-danger" id="'+keys[k]+'"onclick="sil(this)"><i class="fa fa-trash"></i></button></td></tr>'
   toplam += bilgiler.urun_toplami;
 }
 son_odeme.innerHTML += '<tr><td>   </td><td>   </td><td class="text-right"><h4><strong>Toplam: </strong></h4></td><td class="text-center text-danger"><h4><strong>&#8378;'+toplam+'</strong></h4></td>  </tr>'
@@ -33,6 +34,10 @@ function siparis_ekle(bilgiler){
       urun_toplami: urun_toplami,
   }
   window.localStorage.setItem(urun_adi, JSON.stringify(storage));
+      jSuites.notification({
+        name: 'Siparişiniz Eklenmiştir.',
+        message: urun_adi,
+    })
   var goruntu = document.getElementById("siparis_tamamla");
   goruntu.innerHTML = localStorage.length
 }
@@ -42,18 +47,34 @@ function tamamla(){
 
 }
 function siparis_son(){
-  html2canvas(document.getElementById('to_wp'), {
-    onrendered: function(canvas) {
+ // html2canvas(document.getElementById('to_wp'), {
+  //  onrendered: function(canvas) {
       // document.body.appendChild(canvas);
-      return Canvas2Image.saveAsPNG(canvas);
+    //  return Canvas2Image.saveAsPNG(canvas);
       //var dataURL = canvas.toDataURL("image/jpg")
       //dataURL = dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    }
-  });
+ //   }
+ // });
+  adres = document.getElementById("adres").value
+  ad_soyad = document.getElementById("ad_soyad").value
 
-}
-function sil(x){
+  var mesaj = " https://wa.me/905465402741?text=";
+  keys = Object.keys(localStorage);
+  for(var k = 0; k < keys.length; k++){
+    var bilgiler = JSON.parse(`${localStorage.getItem(keys[k])}`);
+    mesaj += " *Urun Adi:* "+ keys[k] + " *Urun Miktarı:* "+(bilgiler.urun_adedi)+"%0a"
+  }
+  mesaj += "*Alici Adi Soyadi:* "+ad_soyad+"%0a" 
+  mesaj += "*Alici Adres:* "+adres+"%0a"
+  mesaj +=" *Toplam Fiyat:* " + toplam 
 
-  window.localStorage.removeItem(x.id);
-  window.location.reload();
+alert(mesaj)
+  location.href = mesaj;
 }
+
+function sil(test){
+
+ window.localStorage.removeItem(test.id);
+ location.reload();
+}
+
