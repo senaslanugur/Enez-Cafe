3351ae7b35ee9bf2a8055b39ca298090fdc14263
senaslanugur <91412306+senaslanugur@users.noreply.github.com>
2021-10-17 11:58:43 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ var data =
       "manu_name":"Karışık Tost Menü",
       "details":"Patates Kızartması ve İçecek",
       "price":25,
-      "photo_name":"http://www.salmanpastanesi.com/Assets/Image/urunler/tost/karisik.png"
+      "photo_name":"https://image.freepik.com/free-photo/club-sandwiches-with-tomato-ketchup-mayonnaise-potatoes_114579-3390.jpg"
     },
     {
       "manu_name":"Kavurmalı Tost Menü",
@@ -17,25 +17,25 @@ var data =
       "manu_name":"Hamburger Menü",
       "details":"Patates Kızartması ve Icecek",
       "price":35,
-      "photo_name":"https://cevatusta.com.tr/wp-content/uploads/2020/12/hamburger-600x400.jpg"
+      "photo_name":"https://image.freepik.com/free-photo/burger-with-meat-tomato-lettuce-served-with-french-fries_114579-2977.jpg"
     },
     {
       "manu_name":"Cheeseburger Menü",
       "details":"Patates Kızartması ve Icecek",
       "price":38,
-      "photo_name":"https://www.demo45.hazirwebsiteler.com/Uploads/Products/thumb/xlg_cheesburher.jpg"
+      "photo_name":"https://image.freepik.com/free-vector/realistic-burger-chalkboard-background_79603-1042.jpg"
     },
     {
       "manu_name":"Patates Kızartması",
       "details":"Patates Kızartması",
       "price":20,
-      "photo_name":"https://i2.milimaj.com/i/milliyet/75/0x410/5f99585cadcdeb046890eb07.jpg"
+      "photo_name":"https://image.freepik.com/free-photo/crispy-french-fries-with-ketchup-mayonnaise_1150-26586.jpg"
     },
     {
       "manu_name":"Soğan Halkası",
       "details":"Soğan Halkası",
       "price":15,
-      "photo_name":"https://cdn.yemek.com/mncrop/940/625/uploads/2019/12/kolay-sogan-halkasi-tarifi.jpg"
+      "photo_name":"https://image.freepik.com/free-photo/onion-rings-wooden-table_144627-16402.jpg"
     }
     ]
 
